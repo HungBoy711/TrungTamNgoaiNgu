bb14b11fd499db4ae90701c067471c44cc68b39b
hungboy711 <hungboy678@gmail.com>
2023-10-17 12:19:44 +0700

    Add project files.

---
+++ TrungTamNgoaiNgu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrungTamNgoaiNgu.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
